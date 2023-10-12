cb48712132afd904e9ff5f256ddecc07be61db5d
Samuel Kimote <readssamuel@.com>
2023-10-12 21:33:48 +0300

    updated with delete functionality

--- Phase 1 Project/index.js
+++ Phase 1 Project/index.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const warehouseList = document.getElementById('warehouseList');
 
     // Function to fetch and display warehouse advertisements
-    // function fetchWarehouseAdvertisements() {
         fetch('http://localhost:3000/warehouses')
             .then(response => response.json())
             .then(data => {
@@ -16,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
                          <p>Description: ${ad.description}</p>
                         <p>Price: $${ad.price}</p>
                         <p>Availability Date: ${ad.availabilityDate}</p>
-                        <button data-id="${ad.id}" class="deleteBtn">Delete</button></br>
+                        <button data-id="${ad.id}" class="deleteBtn">Delete Advertisement</button>
                         </div>`
                         console.log(ad)
                 })
@@ -24,36 +23,57 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Event listener for form submission to add a warehouse advertisement
     warehouseForm.addEventListener('submit', function (e) {
-            e.preventDefault();
+            e.preventDefault(); //Prevents resubmissions 
     
             const image = document.getElementById('image').value;
             const description = document.getElementById('description').value;
             const price = document.getElementById('price').value;
             const availabilityDate = document.getElementById('availabilityDate').value;
-            // Simulated JSON object
+            
             const warehouseData = {
                 image: image,
                 description: description,
                 price: parseFloat(price),
                 availabilityDate: availabilityDate,
             };
-            // Simulated POST request (replace with actual API POST request)
-            fetch('http://localhost:3000/warehouses', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(warehouseData),
+        // POST request
+        fetch('http://localhost:3000/warehouses', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(warehouseData),
             })
-                .then(response => response.json())
-                .then(() => {
-                    // Clear the form and refresh the list
-                    warehouseForm.reset();
-                    warehouseList.innerHTML = ''; // Clear the existing list
-                    fetchWarehouseAdvertisements();
-                });
-     })
-    
+            .then(response => response.json())
+            .then(() => {
+         // Clear the form and refresh the list
+            warehouseForm.reset();
+            warehouseList.innerHTML = ''; // Clears the existing list
+            fetchWarehouseAdvertisements();
+        });
+    })
+
+    // Event listener for a delete button
+    warehouseList.addEventListener('click', function (e) {
+        if (e.target.classList.contains('deleteBtn')) {
+            const adId = e.target.getAttribute('data-id');
+            deleteAdvertisement(adId);
+        }
+    });
+
+    // Function to delete a warehouse advertisement
+    function deleteAdvertisement(adId) {
+        // Delete Request
+        fetch(`http://localhost:3000/warehouses/${adId}`, {
+            method: 'DELETE',
+        })
+        .then(() => {
+            //  list Refreshes after deleting advertisement
+            warehouseList.innerHTML = ''; 
+            fetchWarehouseAdvertisements();
+        })
+    }
+
 
 
 });
