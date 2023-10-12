31dd0d5f95099c432669b09c5b707b8cbc09afb9
Samuel Kimote <readssamuel@.com>
2023-10-12 16:51:36 +0300

    form submission event listener

--- Phase 1 Project/index.js
+++ Phase 1 Project/index.js
@@ -22,7 +22,37 @@ document.addEventListener('DOMContentLoaded', function () {
                 })
             })
 
-    // 
+    // Event listener for form submission to add a warehouse advertisement
+    warehouseForm.addEventListener('submit', function (e) {
+            e.preventDefault();
+    
+            const image = document.getElementById('image').value;
+            const description = document.getElementById('description').value;
+            const price = document.getElementById('price').value;
+            const availabilityDate = document.getElementById('availabilityDate').value;
+            // Simulated JSON object
+            const warehouseData = {
+                image: image,
+                description: description,
+                price: parseFloat(price),
+                availabilityDate: availabilityDate,
+            };
+            // Simulated POST request (replace with actual API POST request)
+            fetch('http://localhost:3000/warehouses', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(warehouseData),
+            })
+                .then(response => response.json())
+                .then(() => {
+                    // Clear the form and refresh the list
+                    warehouseForm.reset();
+                    warehouseList.innerHTML = ''; // Clear the existing list
+                    fetchWarehouseAdvertisements();
+                });
+     })
     
 
 
